9779b616c94ff96eac6882a15550af4923bdee74
Jack Allen <jack.allen@jesus.ox.ac.uk>
2015-05-10 14:39:58 +0100

    add files

--- ONBI_short_project_1/calcCovarianceMatrix.m
+++ ONBI_short_project_1/calcCovarianceMatrix.m
@@ -1,46 +1,21 @@
-function[dia_endo_covariance_matrix, dia_endo_mean_shape, sys_endo_covariance_matrix, sys_endo_mean_shape, sys_myo_covariance_matrix, sys_myo_mean_shape] = calcCovarianceMatrix(data)
+function[dia_endo_covariance_matrix, dia_endo_mean_shape] = calcCovarianceMatrix(data)
 % reshape shape coordinate matrices (to vector form) and find average shape
 %JA 27/04/2015
 
 %% find the mean shape
 dia_endo_sum_of_shapes = zeros(size(data(1).diastolic.endo.xyz(:),1),1);
-sys_endo_sum_of_shapes = zeros(size(data(1).systolic.endo.xyz(:),1),1);
-sys_myo_sum_of_shapes = zeros(size(data(1).systolic.myo.xyz(:),1),1);
 for i = 1:400
-    dia_endo_tmp = data(i).diastolic.endo.xyz(:);
-    sys_endo_tmp = data(i).systolic.endo.xyz(:);
-    sys_myo_tmp = data(i).systolic.myo.xyz(:);
-    
+    dia_endo_tmp = data(1).diastolic.endo.xyz(:);
     dia_endo_sum_of_shapes = dia_endo_tmp + dia_endo_sum_of_shapes;
-    sys_endo_sum_of_shapes = sys_endo_tmp + sys_endo_sum_of_shapes;
-    sys_myo_sum_of_shapes = sys_myo_tmp + sys_myo_sum_of_shapes;
-     
     dia_endo_mean_shape =  dia_endo_sum_of_shapes/400;
-    sys_endo_mean_shape =  sys_endo_sum_of_shapes/400;
-    sys_myo_mean_shape =  sys_myo_sum_of_shapes/400;
-    
 end
 %% find the Covariance matrix
 dia_endo_sum = zeros(size(data(1).diastolic.endo.xyz(:),1));
-sys_endo_sum = zeros(size(data(1).systolic.endo.xyz(:),1));
-sys_myo_sum = zeros(size(data(1).systolic.myo.xyz(:),1));
-
 for n = 1:400
-    %diastolic endo
-    dia_endo_part1 = (data(i).diastolic.endo.xyz(:) - dia_endo_mean_shape);
+    dia_endo_part1 = (data(1).diastolic.endo.xyz(:) - dia_endo_mean_shape);
     dia_endo_part2 = dia_endo_part1.';
     dia_endo_sum = (dia_endo_part1*dia_endo_part2) + dia_endo_sum;
-    %systolic endo
-    sys_endo_part1 = (data(i).systolic.endo.xyz(:) - sys_endo_mean_shape);
-    sys_endo_part2 = sys_endo_part1.';
-    sys_endo_sum = (sys_endo_part1*sys_endo_part2) + sys_endo_sum;
-    %systolic myo
-    sys_myo_part1 = (data(i).systolic.myo.xyz(:) - sys_myo_mean_shape);
-    sys_myo_part2 = sys_myo_part1.';
-    sys_myo_sum = (sys_myo_part1*sys_myo_part2) + sys_myo_sum;
 end
 factor = (1/(400-1));
 dia_endo_covariance_matrix = factor * dia_endo_sum;
-sys_endo_covariance_matrix = factor * sys_endo_sum;
-sys_myo_covariance_matrix = factor * sys_myo_sum;
 end
--- ONBI_short_project_1/calcVolumes.m
+++ ONBI_short_project_1/calcVolumes.m
@@ -1,4 +1,4 @@
-function [sys_epi_volumes, sys_endo_volumes, dia_epi_volumes, dia_endo_volumes] = calcVolumes(data)
+function [sys_epi_volumes, sys_endo_volumes, dia_epi_volumes, dia_endo_volumes, data] = calcVolumes(data)
 
 % Function to calculate the volumes and centers of mass of the different
 % areas of the LV.
@@ -13,11 +13,11 @@ function [sys_epi_volumes, sys_endo_volumes, dia_epi_volumes, dia_endo_volumes] 
 % edges
 for i = 1:400 
 %calculate the hole edge line coordinates xyz B for all patients
-% %diastolic, endo
-% data(i).diastolic.endo.xyz = reshape(data(i).diastolic.endo.xyz, [1089, 3] );
-% data(i).diastolic.epi.xyz = reshape(data(i).diastolic.epi.xyz, [1089, 3] );
-% data(i).systolic.endo.xyz = reshape(data(i).systolic.endo.xyz, [1089, 3] );
-% data(i).systolic.epi.xyz = reshape(data(i).systolic.epi.xyz, [1089, 3] );
+%diastolic, endo
+data(i).diastolic.endo.xyz = reshape(data(i).diastolic.endo.xyz, [1089, 3] );
+data(i).diastolic.epi.xyz = reshape(data(i).diastolic.epi.xyz, [1089, 3] );
+data(i).systolic.endo.xyz = reshape(data(i).systolic.endo.xyz, [1089, 3] );
+data(i).systolic.epi.xyz = reshape(data(i).systolic.epi.xyz, [1089, 3] );
 
 data(i).diastolic.endo.B = vtkFeatureEdges( vtkCleanPolyData(data(i).diastolic.endo) , 'BoundaryEdgesOn' , [] , 'FeatureEdgesOff' , [] );  %extracting the boundary
 data(i).diastolic.endo.B.xyz = data(i).diastolic.endo.B.xyz( [2 1 3:end], : );  %fixing the connectivity.
@@ -103,10 +103,10 @@ data(i).diastolic.endo = FixNormals( data(i).diastolic.endo );
 [data(i).diastolic.endo.volume, data(i).diastolic.endo.centerofmass] = MeshVolume(data(i).diastolic.endo);
 
 %is the calculated volume in the right ball park?
-data(i).systolic.epi.differencevolume = prod(diff( BBMesh( data(i).systolic.epi ) , 1  , 1 ) ) - data(i).systolic.epi.volume;  %%it should be positive!!
-data(i).systolic.endo.differencevolume = prod(diff( BBMesh( data(i).systolic.endo ) , 1  , 1 ) ) - data(i).systolic.endo.volume;  %%it should be positive!!
-data(i).diastolic.epi.differencevolume = prod(diff( BBMesh( data(i).diastolic.epi ) , 1  , 1 ) ) - data(i).diastolic.epi.volume; %%it should be positive!!
-data(i).diastolic.endo.differencevolume = prod(diff( BBMesh( data(i).diastolic.endo ) , 1  , 1 ) ) - data(i).diastolic.endo.volume; %%it should be positive!!
+data(i).systolic.epi.differencevolume = prod(diff( BBMesh( data(i).systolic.epi ) , 1  , 1 ) ) - data(i).systolic.epi.volume;  %%it shoud be positive!!
+data(i).systolic.endo.differencevolume = prod(diff( BBMesh( data(i).systolic.endo ) , 1  , 1 ) ) - data(i).systolic.endo.volume;  %%it shoud be positive!!
+data(i).diastolic.epi.differencevolume = prod(diff( BBMesh( data(i).diastolic.epi ) , 1  , 1 ) ) - data(i).diastolic.epi.volume; %%it shoud be positive!!
+data(i).diastolic.endo.differencevolume = prod(diff( BBMesh( data(i).diastolic.endo ) , 1  , 1 ) ) - data(i).diastolic.endo.volume; %%it shoud be positive!!
 
 %store the difference volumes, in order plot and check are all positive
 % sys_epi_diff_volumes(i,1) = data(i).systolic.epi.differencevolume;
